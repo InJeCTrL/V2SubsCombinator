7fbb38c70fcd5f82de0cded2cefb6c2198ba52ac
InJeCTrL <dap1933@hotmail.com>
2026-03-30 21:29:06 +0800

    fix: ignore extra fields to avoid missing docs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Bson.Serialization;
 using V2SubsCombinator.Database;
 using V2SubsCombinator.IServices;
+using V2SubsCombinator.Models;
 using V2SubsCombinator.Services;
 using V2SubsCombinator.Utils;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 配置 MongoDB 忽略额外字段
+BsonClassMap.RegisterClassMap<User>(cm =>
+{
+    cm.AutoMap();
+    cm.SetIgnoreExtraElements(true);
+});
 
 var jwtHelper = new JWTHelper(builder.Configuration);
 
